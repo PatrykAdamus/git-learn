bd7cd7957edff63c25c8bfd8502cb317d9090ec2
Patryk Adamus <patrykadamus123@gmail.com>
2023-11-02 16:01:15 +0100

    added new method

--- src/pl/adamus/patryk/git/learn/branch/BranchMain.java
+++ src/pl/adamus/patryk/git/learn/branch/BranchMain.java
@@ -3,6 +3,9 @@ package pl.adamus.patryk.git.learn.branch;
 public class BranchMain {
     public static void main(String[] args) {
         System.out.println("Hello Branch");
+    }
+
+    public void change() {
 
     }
 }
